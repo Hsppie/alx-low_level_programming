bf641032282d5e0ece7b4787e0bd5fc8e6083bf3
Hsppie <wambimicheal@gmail.com>
2022-09-14 05:19:58 -0700

    fixed 100 times tabled

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,13 +22,13 @@ void print_times_table(int n)
 				_putchar(32);
 				if (op <= 9)
 				{
-					_putchar('32');
-					_putchar('32');
+					_putchar(32);
+					_putchar(32);
 					_putchar(op + 48);
 				} 
 				else if (op <= 99)
 				{
-					_putchar('32');
+					_putchar(32);
 					_putchar((op / 10) + 48);
 					_putchar((op % 10) + 10);
 				}
