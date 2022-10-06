416cf8c1c7784fa20cc5c59a795ca9e0cd7f1b76
Hsppie <wambimicheal@gmail.com>
2022-10-06 06:10:51 -0700

    fixed betty error

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * main - multiplies two positive numbers
@@ -23,7 +23,7 @@ int i, j;
 		}
 
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
